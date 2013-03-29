b7917d4425f9fa2f268d7147b6752d97d7c11424
Ysta <gustav@alltomminrov.se>
2013-03-30 00:56:06 +0100

    updated migrations

--- db/migrate/20130328111249_create_fantasy_allsvenskan_teams.rb
+++ db/migrate/20130328111249_create_fantasy_allsvenskan_teams.rb
@@ -2,8 +2,10 @@ class CreateFantasyAllsvenskanTeams < ActiveRecord::Migration
   def change
     create_table :fantasy_allsvenskan_teams do |t|
     
-      
-      
+      t.string :name
+      t.integer :id, :null => false
+      t.integer :next_opposition
+      t.integer :previous_opposition
 
       t.timestamps
     end
--- db/migrate/20130329231346_create_fantasy_allsvenskan_users.rb
+++ db/migrate/20130329231346_create_fantasy_allsvenskan_users.rb
@@ -1,9 +1,16 @@
 class CreateFantasyAllsvenskanUsers < ActiveRecord::Migration
   def change
     create_table :fantasy_allsvenskan_users do |t|
-      
+    
+      t.string :username
+      t.string :teamname
+      t.string :password
+      t.integer :id, :auto_increment, :null => false
       t.text :squad
-      
+      t.integer :points
+      t.integer :available_trades
+      t.string :teamcolor
+
       t.timestamps
     end
   end
